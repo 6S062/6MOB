1bdee971537d6f5c5a351525db8af9a798f41ccf
Samuel Madden <madden@csail.mit.edu>
2016-01-23 13:07:34 -0500

    initial rest api

--- anteater-iphone/Anteater/AnteaterREST.h
+++ anteater-iphone/Anteater/AnteaterREST.h
@@ -7,9 +7,14 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "BLESensorReading.h"
+
+#define HTTP_STATUS_OK 200
+#define HTTP_STATUS_DUPLICATE 409
 
 @interface AnteaterREST : NSObject
 
 +(void) getListOfAnthills:(void (^)(NSDictionary *))callback;
-
++(void) postListOfSensorReadings:(NSArray<BLESensorReading *> *)readings andCallCallback:(void (^)(int resultCode, NSDictionary *))callback;
++(void) registerUser:(NSString *)userid withDeviceId:(NSString *)deviceid andCallback:(void (^)(int resultCode, NSDictionary *)) callback;
 @end
