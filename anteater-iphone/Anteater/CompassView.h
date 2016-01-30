ea9aaea7cf07fd188931fd9d7de5e73e07b4763e
Samuel Madden <madden@csail.mit.edu>
2016-01-30 17:02:44 -0500

    refactor a bit

--- anteater-iphone/Anteater/CompassView.h
+++ anteater-iphone/Anteater/CompassView.h
@@ -9,6 +9,15 @@
 #import <UIKit/UIKit.h>
 #import <CoreLocation/CoreLocation.h>
 
-@interface CompassView : UIImageView<CLLocationManagerDelegate>
+#define degToRad(x) (M_PI * (x) / 180.0)
+#define radToDeg(x) ((x) * 180.0 / M_PI)
+
+@interface CompassView : UIImageView
 @property CGFloat rotationOffset;
+@property double lastMagHeading;
+@property CLLocationCoordinate2D userLoc, targetLoc;
+@property IBOutlet UILabel *headingLabel, *distanceLabel;
+
+-(void)rotateArrow;
+
 @end
--- anteater-iphone/Anteater/CompassView.m
+++ anteater-iphone/Anteater/CompassView.m
@@ -10,19 +10,34 @@
 #import "AnteaterREST.h"
 
 
-#define degToRad(x) (M_PI * (x) / 180.0)
-#define radToDeg(x) ((x) * 180.0 / M_PI)
 
 
 @implementation CompassView {
-    double _curHeading;
-    NSArray *_anthills;
-    CLLocationManager *_mgr;
+    CGFloat _rotation, _curHeading, _lastHeading, _scale;
     UIImage *_image;
-    BOOL gotLoc;
-    double _lastHeading, _lastMagHeading;
-    double _scale;
-    CGFloat _rotation;
+}
+
+
+-(id) initWithCoder:(NSCoder *)aDecoder {
+    self = [super initWithCoder:aDecoder];
+    if (self) {
+        self.backgroundColor = [UIColor clearColor];
+        self.layer.anchorPoint = CGPointMake(.5, .5);
+        _scale = 1.0;
+        _image = self.image;
+        //_rotation = 97;
+        //_scale = .4;
+        self.distanceLabel.text = @"";
+        self.headingLabel.text = @"";
+        [self rotateArrow];
+
+        //self.layer.borderWidth = 1.0;
+        //self.layer.borderColor = [[UIColor blackColor] CGColor];
+        
+        
+
+    }
+    return self;
 }
 
 
@@ -38,64 +53,12 @@
     
 }
 
--(id) initWithCoder:(NSCoder *)aDecoder {
-    self = [super initWithCoder:aDecoder];
-    if (self) {
-        _mgr = [[CLLocationManager alloc] init];
-        _mgr.delegate = self;
-        _mgr.desiredAccuracy = kCLLocationAccuracyBest;
-        _mgr.distanceFilter = 0;
-        
-        [_mgr startUpdatingHeading];
-        [_mgr startUpdatingLocation];
-        _anthills = @[];
-        [AnteaterREST getListOfAnthills:^(NSDictionary *hills) {
-            if (hills)
-                _anthills = [hills objectForKey:@"anthils"];
-
-        }];
-        self.backgroundColor = [UIColor clearColor];
-        self.layer.anchorPoint = CGPointMake(.5, .5);
-        _scale = 1.0;
-        _image = self.image;
-        //self.layer.borderWidth = 1.0;
-        //self.layer.borderColor = [[UIColor blackColor] CGColor];
-
-    }
-    return self;
-
-}
 
 -(void)rotateArrow {
-    double head =(_curHeading - _lastMagHeading) + _rotation;
-    NSLog(@"Got heading = %f (estimated direction = %f)", _lastMagHeading,  head);
-    CGAffineTransform t = CGAffineTransformMakeScale(_scale, _scale);
-    //CGAffineTransformMakeTranslation(self.frame.size.width/2, self.frame.size.height/2);
+        
+    _curHeading = [self computeHeadingFromCoordinate:_userLoc toCoordinate:_targetLoc];
     
-    _lastHeading = head; //head * .5 + _lastHeading * .5; //smooth
-    t = CGAffineTransformRotate(t, degToRad(_lastHeading));
-    
-    if (gotLoc)
-        self.transform = t;
-    //if (gotLoc)
-    //    self.image = [_image imageRotatedByDegrees:_curHeading - [newHeading trueHeading]];
-    
-
-}
-
-- (void)locationManager:(CLLocationManager *)manager didUpdateHeading:(CLHeading *)newHeading {
-    _lastMagHeading = [newHeading trueHeading];
-    [self rotateArrow];
-
-}
-
--(void)locationManager:(CLLocationManager*)manager didUpdateLocations:(nonnull NSArray<CLLocation *> *)locations {
-    CLLocationCoordinate2D hill = CLLocationCoordinate2DMake(42.3598, -71.0921);
-    CLLocationCoordinate2D me = [[locations objectAtIndex:0] coordinate];
-    _curHeading = [self computeHeadingFromCoordinate:me toCoordinate:hill];
-    gotLoc = TRUE;
-    
-    CLLocationDistance distanceKm = [[locations objectAtIndex:0] distanceFromLocation:[[CLLocation alloc] initWithLatitude:hill.latitude longitude:hill.longitude]] / 1000.0;
+    CLLocationDistance distanceKm = [[[CLLocation alloc] initWithLatitude:_userLoc.latitude longitude:_userLoc.longitude] distanceFromLocation:[[CLLocation alloc] initWithLatitude:_targetLoc.latitude longitude:_targetLoc.longitude]] / 1000.0;
     if (distanceKm < 2.0) {
         _scale = 1.0 - (pow(1000,(2.0 - distanceKm)/2.0) / 1000.0); //shrink arrow as we get closer
         NSLog(@"Distance = %f, _scale = %f",distanceKm,_scale);
@@ -103,16 +66,34 @@
     } else {
         _scale = 1.0;
     }
-    NSLog(@"estimated heading from loc = %f ", _curHeading);   // scale = 100 - distance
-    //self.image = [_image imageRotatedByDegrees:_curHeading];
+    self.distanceLabel.text = [NSString stringWithFormat:@"%.1f km",distanceKm];
+
+    double head =(_curHeading - _lastMagHeading) + _rotation;
+    if (head < 0) head = head + 360;
+    
+    NSLog(@"Got heading = %f (estimated direction = %f)", _lastMagHeading,  head);
+    CGAffineTransform t = CGAffineTransformMakeScale(_scale, _scale);
+    //CGAffineTransformMakeTranslation(self.frame.size.width/2, self.frame.size.height/2);
+    
+    _lastHeading = head; //head * .5 + _lastHeading * .5; //smooth
+
+    t = CGAffineTransformRotate(t, degToRad(_lastHeading));
+    
+    self.transform = t;
+    self.headingLabel.text = [NSString stringWithFormat:@"%.1f°",head];
+    //if (gotLoc)
+    //    self.image = [_image imageRotatedByDegrees:_curHeading - [newHeading trueHeading]];
+    
+
 }
+
 
 -(CGFloat)rotationOffset {
     return _rotation;
 }
 
 -(void)setRotationOffset:(CGFloat)rotation {
-    _rotation = rotation + 180;
+    _rotation = rotation;
     [self rotateArrow];
     
 }
