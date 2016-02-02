f3d749ecddbf3713695ddd78a884864d46025a17
Samuel Madden <madden@csail.mit.edu>
2016-02-01 22:42:59 -0500

    comments

--- anteater-arduino/ble_temp_sensor/ble_temp_sensor.ino
+++ anteater-arduino/ble_temp_sensor/ble_temp_sensor.ino
@@ -1,7 +1,7 @@
 
 /*
 
-Copyright (c) 2012-2014 RedBearLab
+Copyright (c) 2012-2014 RedBearLab and Sam Madden
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
@@ -12,11 +12,8 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 */
 
 /*
- *    Chat
+ *    App to deliver Temp and Pressure Readings over BLE 
  *
- *    Simple chat sketch, work with the Chat iOS/Android App.
- *    Type something from the Arduino serial monitor to send
- *    to the Chat App or vice verse.
  *
  */
 
@@ -39,7 +36,7 @@ void setup()
   //ble_set_pins(3, 2);
   
   // Set your BLE Shield name here, max. length 10
-  ble_set_name("HILL3");  
+  ble_set_name("HILL6");  
     Serial.print("I'm alive!");
 
 // Call the function to enable low power consumption
